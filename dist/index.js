f3eec9bfb8a69ef6ae1360e35548ae39497fa1c5
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-01-09 23:13:36 +0600

    7. Working With Events

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,12 @@
 "use strict";
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput");
-btn?.addEventListener("click", function () {
-    alert(input.value);
-    input.value = "";
+const form = document.getElementById("todoform");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log("Submitted");
 });
+// btn?.addEventListener("click", function () {
+//   alert(input.value);
+//   input.value = "";
+// });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
+const form = document.getElementById("todoform")!;
 
-btn?.addEventListener("click", function () {
-  alert(input.value);
-  input.value = "";
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("Submitted");
 });
+// btn?.addEventListener("click", function () {
+//   alert(input.value);
+//   input.value = "";
+// });
