b28ebbd405f7b50086232168249a466a92f0125d
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-01-09 23:40:46 +0600

    9. Adding in an Interface

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,26 @@
 "use strict";
+const todos = [];
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput");
 const form = document.getElementById("todoform");
 const list = document.getElementById("todolist");
 function handleSubmit(e) {
     e.preventDefault();
-    const newTodoText = input.value;
+    const newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    input.value = "";
+}
+function createTodo(todo) {
     const newLI = document.createElement("li");
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    newLI.append(newTodoText);
+    newLI.append(todo.text);
     newLI.append(checkbox);
     list.append(newLI);
-    input.value = "";
 }
 form.addEventListener("submit", handleSubmit);
 // btn?.addEventListener("click", function () {
